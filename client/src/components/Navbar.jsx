e6ccca78f796f3e962af15ddc2423971014deae7
srinjoymahanti <srinjoymahanti@gmail.com>
2025-08-23 11:08:41 +0530

    refactor Navbar component to improve user state handling and layout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React,{useState} from 'react'
 import {assets} from '../assets/assets'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 
 const Navbar = () => {
-    const [user,setUser]=useState(null);
+    const [user,setUser]=useState(true);
+    const navigate=useNavigate();
   return (
     <div className='flex justify-between items-center py-4'>
         <Link to='/'>
@@ -12,11 +13,26 @@ const Navbar = () => {
         <div>
             {
             user ?
-            <div></div>
-            :
             <div>
-                <p>Pricing</p>
-                <button>Login</button>
+              <button>
+                <img className='w-5' src={assets.credit_star} alt=''/>
+                <p>Credits left:50</p>
+              </button>
+              <p>Hi welcome</p>
+              <div className='relative group'>
+                <img className='w-10 drop-shadow' src={assets.profile_icon} alt=''/>
+                <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
+                  <ul>
+                    <li>Logout</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            :
+            <div className='flex items-center gap-2 sm:gap-5'>
+                <p onClick={()=>navigate('/buy')}
+                className='cursor-pointer'>Pricing</p>
+                <button className='bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full'>Login</button>
             </div>
             }
         </div>
